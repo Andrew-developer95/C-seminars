42f3bc2a2f5764a5c6d51b83bf744a56070828c9
Лефтер Андрей <andrew299347@gmail.com>
2022-09-04 23:46:07 +0200

    Seminars and hw1

---
+++ Seminar 1/Task_7/Program.cs
@@ -0,0 +1,16 @@
+﻿// Напишите программу, которая на вход принимает трехзначное число
+// и на выходе показывает последнюю цифру этого числа.
+// 456 -> 6
+// 782 -> 2
+
+Console.WriteLine("Введите трехзначное число: ");
+int number = Convert.ToInt32(Console.ReadLine());
+if (number > 99 && number < 1000)
+{
+    int count = number % 10;
+    Console.WriteLine(count);
+}
+else 
+{
+  Console.WriteLine("Введено не трехзначное число!");
+}
---
+++ hw_Seminar1/Task8/Program.cs
@@ -0,0 +1,15 @@
+﻿//Задача 8: Напишите программу, которая на вход принимает число (N),
+//а на выходе показывает все чётные числа от 1 до N.
+//5 -> 2, 4
+//8 -> 2, 4, 6, 8
+// done
+
+Console.WriteLine("Введите число: ");
+int number = Convert.ToInt32(Console.ReadLine());
+int currentValue = 2;
+
+while (currentValue <= number)
+{
+  Console.Write(currentValue + " ");
+  currentValue += 2; //currentValue = currentValue + 2 
+}
